e3024b244d5858aaf27e4fa1e399891410b20ff2
Edijs Kolesnikovičs <terminedijs@yahoo.com>
2022-10-08 18:08:14 +0300

    Peržiūrėti leaderboardą ir norint žaisti, reikia būti prisijungus

--- application/urls.py
+++ application/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 from .views import index, sign_up, user_account, RetrieveLeaderboardView
 #  RetrieveOrdersView, CreateOrderView, UpdateOrderView, DeleteOrderView
+from django.contrib.auth.decorators import login_required
 
 app_name = 'application'  # namespace
 
 urlpatterns = [
     path('', index, name='index'),
-    path('leaderboard/', RetrieveLeaderboardView.as_view(), name='leaderboard'),
-    # path('leaderboard/', RetrieveLeaderboardView.as_view(), name='leaderboard'),
+    path('leaderboard/', login_required(RetrieveLeaderboardView.as_view()), name='leaderboard'),
+    path('play/', login_required(RetrieveLeaderboardView.as_view()), name='play'),
     # path('orders/create/', CreateOrderView.as_view(), name='order_create'),
     # path('orders/<int:pk>/update/', UpdateOrderView.as_view(), name='order_update'),
     # path('orders/<int:pk>/delete/', DeleteOrderView.as_view(), name='order_delete'),
     path('registration/sign_up', sign_up, name='sign_up'),
-    path('user_account', user_account, name='user_account'),
+    path('user_account', login_required(user_account), name='user_account'),
 ]
